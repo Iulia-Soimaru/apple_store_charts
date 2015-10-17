e990ba5b3e30f9b8e1dff5cbd27689e3f7876a53
Iulia Soimaru <iuliashoimaru@gmail.com>
2015-10-16 17:11:37 -0700

    try to fix loading page problem

--- app/controllers/mobile_apps_controller.rb
+++ app/controllers/mobile_apps_controller.rb
@@ -3,28 +3,29 @@ class MobileAppsController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.json {render :json => MobileApp.all_apps(params[:offset])}
+      format.json {render :json => MobileApp.all_apps(params[:offset]), :status => 200}
     end
+    # render status: 200, json: MobileApp.all_apps(params[:offset])
   end
 
   def top_paid
     respond_to do |format|
       format.html
-      format.json {render :json => MobileApp.paid_apps(params[:offset])}
+      format.json {render :json => MobileApp.paid_apps(params[:offset]), :status => 200}
     end
   end
 
   def top_free
     respond_to do |format|
       format.html
-      format.json {render :json => MobileApp.free_apps(params[:offset])}
+      format.json {render :json => MobileApp.free_apps(params[:offset]), :status => 200}
     end
   end
 
   def top_grossing
     respond_to do |format|
       format.html
-      format.json {render :json => MobileApp.grossing_apps(params[:offset])}
+      format.json {render :json => MobileApp.grossing_apps(params[:offset]), :status => 200}
     end
   end
 
